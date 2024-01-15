dcf15b97dc31a1c03eddcec5d3d50c1698094f17
Mersha261 <50096806+Mersha261@users.noreply.github.com>
2024-01-15 21:54:29 +0330

    second Commit

--- ProgramingCalssProject/Program.cs
+++ ProgramingCalssProject/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using ProgramingCalssProject.Data;
+using PgrogrammingClass.Data.DataContext;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +11,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
---
+++ ProgrammingCalss.Data/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PgrogrammingClass.Core.Domain;
+
+namespace PgrogrammingClass.Data.DataContext
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            :base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial catalog=ProgrammingCalssProject;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<TblBanner> TblBanner { get; set; }
+    }
+}
