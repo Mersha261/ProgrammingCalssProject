1671d205071efb59ef37632f329246c30df9e502
Mersha261 <50096806+Mersha261@users.noreply.github.com>
2024-01-15 21:12:47 +0330

    Initial commit

---
+++ PgrogrammingClass.Core/Domain/TblBanner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PgrogrammingClass.Core.Domain
+{
+    public class TblBanner
+    {
+        public int Id { get; set; }
+        public string Picture { get; set; }
+        public string Title { get; set; }
+        public string Alt { get; set; }
+
+    }
+}
---
+++ ProgramingCalssProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProgramingCalssProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
+        //{
+        //    optionbuilder.UseSqlServer("Server=.;initial catalog=DatabaseName;Trusted_Connection=True;MultipleActiveResultSets=true");
+        //}
+    }
+}
